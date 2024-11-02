7b646a16b55ecd0ce1e74cac0e735a55827b39cc
Mikicrep <mikicrepoffical@gmail.com>
2024-11-02 21:22:40 +0100

    Initilize font

--- miki-framework/core/init.cpp
+++ miki-framework/core/init.cpp
@@ -11,6 +11,9 @@ namespace core {
 
         // Init everything
         SDL_Init(SDL_INIT_EVERYTHING);
+	    TTF_Init();
+
+        sdlSettings.font = TTF_OpenFont("mf/font.ttf", 96);
 
         // Create window and renderer
         sdlSettings.window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, sdlSettings.width, sdlSettings.height, 0);
