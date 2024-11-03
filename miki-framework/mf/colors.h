c44fb75dca2fb9a0de55b11bb39d3a51d770f7ba
Mikicrep <mikicrepoffical@gmail.com>
2024-11-03 12:47:23 +0100

    Document colors.h

--- miki-framework/mf/colors.h
+++ miki-framework/mf/colors.h
@@ -1,8 +1,15 @@
+/**
+ * @brief This namespace contains colors for various elements
+ */
+
 #pragma once
 
 #include "SDL.h"
 
 namespace colors {
+    /**
+     * @brief Colors for texts
+     */
     namespace text {
 	    constexpr SDL_Color blackTextColor = {0, 0, 0};
         constexpr SDL_Color whiteTextColor = {255, 255, 255};
