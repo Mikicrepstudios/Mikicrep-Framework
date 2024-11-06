dd1cbba52136eb03edbc74ac100659118c59842d
Mikicrep <mikicrepoffical@gmail.com>
2024-11-07 00:48:33 +0100

    Added mouse detection

--- miki-framework/core/miki.cpp
+++ miki-framework/core/miki.cpp
@@ -3,6 +3,7 @@
 #include "mf/core.h"
 #include "mf/colors.h"
 #include "mf/graphics.h"
+#include "mf/logic.h"
 
 namespace core {
     void miki(core::SDL_Settings sdlSettings) {
@@ -19,5 +20,12 @@ namespace core {
         draw::DrawTransRect(sdlSettings.renderer, rect1, 255, 0, 0, 255);
         draw::DrawTransRect(sdlSettings.renderer, rect2, 0, 0, 255, 150);
         draw::DrawTransRect(sdlSettings.renderer, rect3, 0, 255, 0, 150);
+
+        if(logic::IsMouseTouching(sdlSettings.mouseX, sdlSettings.mouseY, rect3))
+            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, 0, 255, 0);
+        else if(logic::IsMouseTouching(sdlSettings.mouseX, sdlSettings.mouseY, rect2))
+            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, 0, 0, 255);
+        else if(logic::IsMouseTouching(sdlSettings.mouseX, sdlSettings.mouseY, rect1))
+            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, 255, 0, 0);
     }
 }
--- miki-framework/mf/logic.h
+++ miki-framework/mf/logic.h
@@ -4,5 +4,6 @@
 
 namespace logic {
     int GenRanNum(int min, int max);
-    bool AreRectsTouching(const SDL_Rect& rect1, const SDL_Rect& rect2);
+    bool AreRectsTouching(const SDL_Rect& rect1, SDL_Rect& rect2);
+    bool IsMouseTouching(int mouseX, int mouseY, SDL_Rect& rect);
 }
