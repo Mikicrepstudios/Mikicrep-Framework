a9db906ce22995c1ba7550844a621e94f7eeb3e8
Mikicrep <mikicrepoffical@gmail.com>
2024-11-07 00:48:51 +0100

    Gen docs

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['arerectstouching_0',['AreRectsTouching',['../d7/df0/namespacelogic.html#ac6fb740bf4e4b9358352f3c5cd38fb34',1,'logic']]],
+  ['arerectstouching_0',['AreRectsTouching',['../d7/df0/namespacelogic.html#a45419233f017d9dd624f403feae8b266',1,'logic']]],
   ['arerectstouching_2ecpp_1',['arerectstouching.cpp',['../d7/de4/arerectstouching_8cpp.html',1,'']]]
 ];
--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['initwindow_0',['InitWindow',['../d9/d0f/namespacecore.html#a85c483d6350b82caf16dae1e3c9dfc4f',1,'core']]]
+  ['initwindow_0',['InitWindow',['../d9/d0f/namespacecore.html#a85c483d6350b82caf16dae1e3c9dfc4f',1,'core']]],
+  ['ismousetouching_1',['IsMouseTouching',['../d7/df0/namespacelogic.html#a280be3d8537425d568759dd95a3c801c',1,'logic']]]
 ];
