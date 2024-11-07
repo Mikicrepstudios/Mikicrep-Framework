68dbb64d855828b72e03ab986062178cc8f7eb21
Mikicrep <mikicrepoffical@gmail.com>
2024-11-07 01:02:22 +0100

    Document loadimg.cpp

--- miki-framework/core/loadimg.cpp
+++ miki-framework/core/loadimg.cpp
@@ -6,6 +6,12 @@
 
 namespace core {
     SDL_Texture* LoadImg(SDL_Renderer* renderer, std::string path) {
+        /**
+         * @brief This function loads image and returns it as SDL_Texture*
+         * @param renderer Renderer of your window
+         * @param path Path to image (recommended to use / for paths)
+         */
+        
         SDL_Surface* tmpSurface = IMG_Load(path.c_str());
         SDL_Texture* tmpTexture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
         SDL_FreeSurface(tmpSurface);
