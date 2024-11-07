512ea3ef3d4378bed16d1964e866a28698834a9a
Mikicrep <mikicrepoffical@gmail.com>
2024-11-07 23:56:43 +0100

    Print version on start

--- source/main.cpp
+++ source/main.cpp
@@ -8,6 +8,7 @@
 
 int main(int argc, char* argv[]) {
     std::cout << "-------Mikicrep Framework-------" << std::endl
+              << "------------Ver: DEV------------" << std::endl
               << "Copyright Mikicrep Studios 2024" << std::endl;
 
     // Main stuff
