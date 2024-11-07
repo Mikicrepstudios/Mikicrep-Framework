b0f282e5d30f2451bad76f2f2870d64e7879a880
Mikicrep <mikicrepoffical@gmail.com>
2024-11-07 23:49:26 +0100

    FInishing new color system

--- miki-framework/core/miki.cpp
+++ miki-framework/core/miki.cpp
@@ -17,15 +17,15 @@ namespace core {
         SDL_Rect rect2 = {75, 50, 50, 50};
         SDL_Rect rect3 = {62, 75, 50, 50};
 
-        draw::DrawTransRect(sdlSettings.renderer, rect1, 255, 0, 0, 255);
-        draw::DrawTransRect(sdlSettings.renderer, rect2, 0, 0, 255, 150);
-        draw::DrawTransRect(sdlSettings.renderer, rect3, 0, 255, 0, 150);
+        draw::DrawTransRect(sdlSettings.renderer, rect1, colors::general::red, 255);
+        draw::DrawTransRect(sdlSettings.renderer, rect2, colors::general::indigo, 150);
+        draw::DrawTransRect(sdlSettings.renderer, rect3, colors::general::grassgreen, 150);
 
         if(logic::IsMouseTouching(sdlSettings.mouseX, sdlSettings.mouseY, rect3))
-            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, 0, 255, 0);
+            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, colors::general::grassgreen);
         else if(logic::IsMouseTouching(sdlSettings.mouseX, sdlSettings.mouseY, rect2))
-            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, 0, 0, 255);
+            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, colors::general::indigo);
         else if(logic::IsMouseTouching(sdlSettings.mouseX, sdlSettings.mouseY, rect1))
-            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, 255, 0, 0);
+            draw::DrawRect(sdlSettings.renderer, {sdlSettings.width - 50, sdlSettings.height - 50, 50, 50}, colors::general::red);
     }
 }
