4c01395d33fac4f912ba46e4f1f6390ae6298d11
Mikicrep <mikicrepoffical@gmail.com>
2024-11-07 23:10:17 +0100

    Starting to add predefined colors

--- miki-framework/mf/colors.h
+++ miki-framework/mf/colors.h
@@ -6,7 +6,20 @@
 
 #include "SDL.h"
 
+struct MF_Color {
+    int r = 0;
+    int g = 0;
+    int b = 0;
+};
+
 namespace colors {
+    /**
+     * @brief General colors
+     */
+    namespace general {
+        constexpr MF_Color white = {255, 255, 255};
+    };
+
     /**
      * @brief Colors for texts
      */
