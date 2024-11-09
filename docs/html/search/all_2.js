93b31f2fea5ee549b433dfcffb8b46ce0bc8f07a
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 20:59:15 +0100

    Update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -8,6 +8,8 @@ var searchData=
   ['colors_3a_3atext_5',['text',['../d9/da7/namespacecolors_1_1text.html',1,'colors']]],
   ['core_6',['core',['../d9/d0f/namespacecore.html',1,'']]],
   ['core_2eh_7',['core.h',['../d5/dc4/core_8h.html',1,'']]],
-  ['crimsonred_8',['crimsonred',['../de/d3d/namespacecolors_1_1general.html#a4c50d22adfed2ecb1d43f0b102a404c1',1,'colors::general']]],
-  ['cyan_9',['cyan',['../de/d3d/namespacecolors_1_1general.html#a306de46585e4da7953eb246aab8e4f9c',1,'colors::general']]]
+  ['createfile_8',['CreateFile',['../d6/d04/namespacefiles.html#a0dd1df757d65d4a42f2c3dde0913fe2c',1,'files']]],
+  ['createfile_2ecpp_9',['createfile.cpp',['../d4/dad/createfile_8cpp.html',1,'']]],
+  ['crimsonred_10',['crimsonred',['../de/d3d/namespacecolors_1_1general.html#a4c50d22adfed2ecb1d43f0b102a404c1',1,'colors::general']]],
+  ['cyan_11',['cyan',['../de/d3d/namespacecolors_1_1general.html#a306de46585e4da7953eb246aab8e4f9c',1,'colors::general']]]
 ];
