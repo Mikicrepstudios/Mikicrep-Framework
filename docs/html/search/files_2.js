ee387736d5fb12cef7112559023b69a92e854f72
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 21:06:38 +0100

    Update docs

--- docs/html/search/files_2.js
+++ docs/html/search/files_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['draw_2ecpp_0',['draw.cpp',['../da/d9f/draw_8cpp.html',1,'']]]
+  ['deletefile_2ecpp_0',['deletefile.cpp',['../d0/dae/deletefile_8cpp.html',1,'']]],
+  ['draw_2ecpp_1',['draw.cpp',['../da/d9f/draw_8cpp.html',1,'']]]
 ];
