da8fce231d7ed3a7039e947abfb0e91893545724
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 22:46:15 +0100

    Make error logging say that MF made error

--- miki-framework/sfx/playbgmusic.cpp
+++ miki-framework/sfx/playbgmusic.cpp
@@ -6,13 +6,13 @@ namespace sfx {
     void PlayBGMusic(std::string path) {
         Mix_Music* music = Mix_LoadMUS(path.c_str());
         if (!music) {
-            std::cerr << "Failed to load music file '" << path << "'. SDL_mixer Error: " << Mix_GetError() << '\n';
+            std::cerr << "[MF] Failed to load music file '" << path << "'. SDL_mixer Error: " << Mix_GetError() << '\n';
             return;
         }
 
         if (Mix_PlayMusic(music, -1) == -1) {
-            std::cerr << "Failed to play music. SDL_mixer Error: " << Mix_GetError() << '\n';
-            Mix_FreeMusic(music);  // Free the loaded music if playback fails
+            std::cerr << "[MF] Failed to play music. SDL_mixer Error: " << Mix_GetError() << '\n';
+            Mix_FreeMusic(music);
             return;
         }
     }
