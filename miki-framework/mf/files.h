adbb3ed5a82d1c88b4f5d6420a2e38624764cc07
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 20:58:39 +0100

    Create file function

--- miki-framework/mf/files.h
+++ miki-framework/mf/files.h
@@ -6,6 +6,8 @@
  * @brief This struct contains all basic functions for file managment
  */
 namespace files {
+    int CreateFile(std::string path);
+
     bool CheckFile(std::string path);
     std::string ReadFile(std::string path);
 }
