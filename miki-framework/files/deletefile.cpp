2036e65bcfe1b4588d55bdb81d81c6d19ea9d638
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 21:06:29 +0100

    Document deletefile.cpp

--- miki-framework/files/deletefile.cpp
+++ miki-framework/files/deletefile.cpp
@@ -3,6 +3,11 @@
 
 namespace files {
     int DeleteFile(std::string path) {
+        /**
+         * @brief This function deletes specified file
+         * @param path Path of file that you want to delete
+         */
+        
         if (std::remove(path.c_str()) == 0) return 0;
         else return 1;
 
