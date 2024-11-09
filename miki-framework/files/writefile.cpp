ea3693c0bc7f7dafce94439f687a243d72de4cfc
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 21:19:59 +0100

    Document writefile.cpp

--- miki-framework/files/writefile.cpp
+++ miki-framework/files/writefile.cpp
@@ -3,6 +3,12 @@
 
 namespace files {
     void WriteFile(std::string path, std::string content) {
+        /**
+         * @brief This function writes text to file with overwriting existing one
+         * @param path Path to the file
+         * @param content Stuff that you want to write to the file
+         */
+        
         std::ofstream file(path);
         file << content;
         file.close();
