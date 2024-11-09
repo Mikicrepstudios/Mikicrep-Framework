1190a3228ffd25008c82a2cac833a1cc330dca60
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 20:59:15 +0100

    Update docs

--- docs/html/search/files_1.js
+++ docs/html/search/files_1.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['checkfile_2ecpp_0',['checkfile.cpp',['../d6/dc7/checkfile_8cpp.html',1,'']]],
   ['colors_2eh_1',['colors.h',['../da/dd8/colors_8h.html',1,'']]],
-  ['core_2eh_2',['core.h',['../d5/dc4/core_8h.html',1,'']]]
+  ['core_2eh_2',['core.h',['../d5/dc4/core_8h.html',1,'']]],
+  ['createfile_2ecpp_3',['createfile.cpp',['../d4/dad/createfile_8cpp.html',1,'']]]
 ];
--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['checkfile_0',['CheckFile',['../d6/d04/namespacefiles.html#a43e569765d76cbc3b55feffd40d3a420',1,'files']]]
+  ['checkfile_0',['CheckFile',['../d6/d04/namespacefiles.html#a43e569765d76cbc3b55feffd40d3a420',1,'files']]],
+  ['createfile_1',['CreateFile',['../d6/d04/namespacefiles.html#a0dd1df757d65d4a42f2c3dde0913fe2c',1,'files']]]
 ];
