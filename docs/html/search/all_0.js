e908dfcc54202805b7b2163d6e32ab1adf07f369
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 21:15:53 +0100

    Update docs

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,6 +1,8 @@
 var searchData=
 [
-  ['aqua_0',['aqua',['../de/d3d/namespacecolors_1_1general.html#a08523dd573541241a395ad6a9ab80ef3',1,'colors::general']]],
-  ['arerectstouching_1',['AreRectsTouching',['../d7/df0/namespacelogic.html#a45419233f017d9dd624f403feae8b266',1,'logic']]],
-  ['arerectstouching_2ecpp_2',['arerectstouching.cpp',['../d7/de4/arerectstouching_8cpp.html',1,'']]]
+  ['appendfile_0',['AppendFile',['../d6/d04/namespacefiles.html#a0066aebcde54e5d909725f9a80e6a9bc',1,'files']]],
+  ['appendfile_2ecpp_1',['appendfile.cpp',['../dd/d2d/appendfile_8cpp.html',1,'']]],
+  ['aqua_2',['aqua',['../de/d3d/namespacecolors_1_1general.html#a08523dd573541241a395ad6a9ab80ef3',1,'colors::general']]],
+  ['arerectstouching_3',['AreRectsTouching',['../d7/df0/namespacelogic.html#a45419233f017d9dd624f403feae8b266',1,'logic']]],
+  ['arerectstouching_2ecpp_4',['arerectstouching.cpp',['../d7/de4/arerectstouching_8cpp.html',1,'']]]
 ];
--- docs/html/search/files_0.js
+++ docs/html/search/files_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['arerectstouching_2ecpp_0',['arerectstouching.cpp',['../d7/de4/arerectstouching_8cpp.html',1,'']]]
+  ['appendfile_2ecpp_0',['appendfile.cpp',['../dd/d2d/appendfile_8cpp.html',1,'']]],
+  ['arerectstouching_2ecpp_1',['arerectstouching.cpp',['../d7/de4/arerectstouching_8cpp.html',1,'']]]
 ];
