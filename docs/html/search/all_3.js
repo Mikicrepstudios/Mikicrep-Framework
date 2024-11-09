cd74f5ad5d7983c6caed072a57b9ac182497c858
Mikicrep <mikicrepoffical@gmail.com>
2024-11-09 21:06:38 +0100

    Update docs

--- docs/html/search/all_3.js
+++ docs/html/search/all_3.js
@@ -4,11 +4,13 @@ var searchData=
   ['darkgray_1',['darkgray',['../de/d3d/namespacecolors_1_1general.html#af7c22a0c3bbb1e3a9cdf81f5bd2fe8f1',1,'colors::general']]],
   ['darkgreen_2',['darkgreen',['../de/d3d/namespacecolors_1_1general.html#af9c29f1b7f474b869b76bc68931f3b2d',1,'colors::general']]],
   ['darkred_3',['darkred',['../de/d3d/namespacecolors_1_1general.html#aacf9922b5c91523ff0462293b3b9c773',1,'colors::general']]],
-  ['draw_4',['draw',['../de/d00/namespacedraw.html',1,'']]],
-  ['draw_2ecpp_5',['draw.cpp',['../da/d9f/draw_8cpp.html',1,'']]],
-  ['drawbutton_6',['DrawButton',['../de/d00/namespacedraw.html#a6bb79d11848aa97911775cdd12fca90f',1,'draw']]],
-  ['drawrect_7',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
-  ['drawtext_8',['DrawText',['../de/d00/namespacedraw.html#a1b9dfd5f904ba3b9d931e43b9f143c75',1,'draw']]],
-  ['drawtexturerect_9',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
-  ['drawtransrect_10',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
+  ['deletefile_4',['DeleteFile',['../d6/d04/namespacefiles.html#a6fa94beb256aab79f0c4c86dc82f62a0',1,'files']]],
+  ['deletefile_2ecpp_5',['deletefile.cpp',['../d0/dae/deletefile_8cpp.html',1,'']]],
+  ['draw_6',['draw',['../de/d00/namespacedraw.html',1,'']]],
+  ['draw_2ecpp_7',['draw.cpp',['../da/d9f/draw_8cpp.html',1,'']]],
+  ['drawbutton_8',['DrawButton',['../de/d00/namespacedraw.html#a6bb79d11848aa97911775cdd12fca90f',1,'draw']]],
+  ['drawrect_9',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
+  ['drawtext_10',['DrawText',['../de/d00/namespacedraw.html#a1b9dfd5f904ba3b9d931e43b9f143c75',1,'draw']]],
+  ['drawtexturerect_11',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
+  ['drawtransrect_12',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
 ];
--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['drawbutton_0',['DrawButton',['../de/d00/namespacedraw.html#a6bb79d11848aa97911775cdd12fca90f',1,'draw']]],
-  ['drawrect_1',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
-  ['drawtext_2',['DrawText',['../de/d00/namespacedraw.html#a1b9dfd5f904ba3b9d931e43b9f143c75',1,'draw']]],
-  ['drawtexturerect_3',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
-  ['drawtransrect_4',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
+  ['deletefile_0',['DeleteFile',['../d6/d04/namespacefiles.html#a6fa94beb256aab79f0c4c86dc82f62a0',1,'files']]],
+  ['drawbutton_1',['DrawButton',['../de/d00/namespacedraw.html#a6bb79d11848aa97911775cdd12fca90f',1,'draw']]],
+  ['drawrect_2',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
+  ['drawtext_3',['DrawText',['../de/d00/namespacedraw.html#a1b9dfd5f904ba3b9d931e43b9f143c75',1,'draw']]],
+  ['drawtexturerect_4',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
+  ['drawtransrect_5',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
 ];
