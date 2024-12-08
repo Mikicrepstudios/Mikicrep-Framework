8282f2d96e69cff25b470a14314379f4772be484
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:35:56 +0100

    Update docs

--- docs/html/search/functions_1.js
+++ docs/html/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['checkfile_0',['CheckFile',['../d6/d04/namespacefiles.html#a43e569765d76cbc3b55feffd40d3a420',1,'files']]],
-  ['createfile_1',['CreateFile',['../d6/d04/namespacefiles.html#a0dd1df757d65d4a42f2c3dde0913fe2c',1,'files']]]
+  ['chancegen_0',['chancegen',['../d7/df0/namespacelogic.html#a63fb3df46ba93a8fb47414366cb920b7',1,'logic']]],
+  ['checkfile_1',['CheckFile',['../d6/d04/namespacefiles.html#a43e569765d76cbc3b55feffd40d3a420',1,'files']]],
+  ['createfile_2',['CreateFile',['../d6/d04/namespacefiles.html#a0dd1df757d65d4a42f2c3dde0913fe2c',1,'files']]]
 ];
