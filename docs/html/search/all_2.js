2e98093f8859359f7f361ba02f8362f83a9bc097
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:35:56 +0100

    Update docs

--- docs/html/search/all_2.js
+++ docs/html/search/all_2.js
@@ -1,14 +1,16 @@
 var searchData=
 [
-  ['checkfile_0',['CheckFile',['../d6/d04/namespacefiles.html#a43e569765d76cbc3b55feffd40d3a420',1,'files']]],
-  ['checkfile_2ecpp_1',['checkfile.cpp',['../d6/dc7/checkfile_8cpp.html',1,'']]],
-  ['colorid_2',['colorID',['../dc/d04/namespacecolors.html#a3076012de9738bd1b5186a1d54458dbb',1,'colors']]],
-  ['colors_3',['colors',['../dc/d04/namespacecolors.html',1,'']]],
-  ['colors_2eh_4',['colors.h',['../da/dd8/colors_8h.html',1,'']]],
-  ['core_5',['core',['../d9/d0f/namespacecore.html',1,'']]],
-  ['core_2eh_6',['core.h',['../d5/dc4/core_8h.html',1,'']]],
-  ['createfile_7',['CreateFile',['../d6/d04/namespacefiles.html#a0dd1df757d65d4a42f2c3dde0913fe2c',1,'files']]],
-  ['createfile_2ecpp_8',['createfile.cpp',['../d4/dad/createfile_8cpp.html',1,'']]],
-  ['crimsonred_9',['crimsonred',['../dc/d04/namespacecolors.html#a7faedb7c5de52f634c64a7340ac83aee',1,'colors']]],
-  ['cyan_10',['cyan',['../dc/d04/namespacecolors.html#a853135c6f1188fe504767b9486ff3959',1,'colors']]]
+  ['chancegen_0',['chancegen',['../d7/df0/namespacelogic.html#a63fb3df46ba93a8fb47414366cb920b7',1,'logic']]],
+  ['chancegen_2ecpp_1',['chancegen.cpp',['../d6/da7/chancegen_8cpp.html',1,'']]],
+  ['checkfile_2',['CheckFile',['../d6/d04/namespacefiles.html#a43e569765d76cbc3b55feffd40d3a420',1,'files']]],
+  ['checkfile_2ecpp_3',['checkfile.cpp',['../d6/dc7/checkfile_8cpp.html',1,'']]],
+  ['colorid_4',['colorID',['../dc/d04/namespacecolors.html#a3076012de9738bd1b5186a1d54458dbb',1,'colors']]],
+  ['colors_5',['colors',['../dc/d04/namespacecolors.html',1,'']]],
+  ['colors_2eh_6',['colors.h',['../da/dd8/colors_8h.html',1,'']]],
+  ['core_7',['core',['../d9/d0f/namespacecore.html',1,'']]],
+  ['core_2eh_8',['core.h',['../d5/dc4/core_8h.html',1,'']]],
+  ['createfile_9',['CreateFile',['../d6/d04/namespacefiles.html#a0dd1df757d65d4a42f2c3dde0913fe2c',1,'files']]],
+  ['createfile_2ecpp_10',['createfile.cpp',['../d4/dad/createfile_8cpp.html',1,'']]],
+  ['crimsonred_11',['crimsonred',['../dc/d04/namespacecolors.html#a7faedb7c5de52f634c64a7340ac83aee',1,'colors']]],
+  ['cyan_12',['cyan',['../dc/d04/namespacecolors.html#a853135c6f1188fe504767b9486ff3959',1,'colors']]]
 ];
--- docs/html/search/files_1.js
+++ docs/html/search/files_1.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['checkfile_2ecpp_0',['checkfile.cpp',['../d6/dc7/checkfile_8cpp.html',1,'']]],
-  ['colors_2eh_1',['colors.h',['../da/dd8/colors_8h.html',1,'']]],
-  ['core_2eh_2',['core.h',['../d5/dc4/core_8h.html',1,'']]],
-  ['createfile_2ecpp_3',['createfile.cpp',['../d4/dad/createfile_8cpp.html',1,'']]]
+  ['chancegen_2ecpp_0',['chancegen.cpp',['../d6/da7/chancegen_8cpp.html',1,'']]],
+  ['checkfile_2ecpp_1',['checkfile.cpp',['../d6/dc7/checkfile_8cpp.html',1,'']]],
+  ['colors_2eh_2',['colors.h',['../da/dd8/colors_8h.html',1,'']]],
+  ['core_2eh_3',['core.h',['../d5/dc4/core_8h.html',1,'']]],
+  ['createfile_2ecpp_4',['createfile.cpp',['../d4/dad/createfile_8cpp.html',1,'']]]
 ];
