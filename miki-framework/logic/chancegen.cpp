b6cdfcebade89f3ac4bdce96b862975af11d78de
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:35:26 +0100

    Document chancegen.cpp

--- miki-framework/logic/chancegen.cpp
+++ miki-framework/logic/chancegen.cpp
@@ -2,6 +2,11 @@
 
 namespace logic {
     bool chancegen(int chance) {
+        /**
+         * @brief This function got chance to give true
+         * @param chance What chance to return true (1 in {chance})
+         */
+
         int number = GenRanNum(1, chance);
 
         if(number == 1) return true;
