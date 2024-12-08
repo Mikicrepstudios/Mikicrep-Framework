6654ab84383bb73f5b611d184c89c30cd23968f9
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:18:28 +0100

    Update docs

--- docs/html/search/files_2.js
+++ docs/html/search/files_2.js
@@ -1,5 +1,10 @@
 var searchData=
 [
   ['deletefile_2ecpp_0',['deletefile.cpp',['../d0/dae/deletefile_8cpp.html',1,'']]],
-  ['draw_2ecpp_1',['draw.cpp',['../da/d9f/draw_8cpp.html',1,'']]]
+  ['drawbutton_2ecpp_1',['drawbutton.cpp',['../da/d6c/drawbutton_8cpp.html',1,'']]],
+  ['drawrect_2ecpp_2',['drawrect.cpp',['../d1/da5/drawrect_8cpp.html',1,'']]],
+  ['drawshakingrect_2ecpp_3',['drawshakingrect.cpp',['../d5/d8c/drawshakingrect_8cpp.html',1,'']]],
+  ['drawtext_2ecpp_4',['drawtext.cpp',['../d7/dc8/drawtext_8cpp.html',1,'']]],
+  ['drawtexturerect_2ecpp_5',['drawtexturerect.cpp',['../db/dbf/drawtexturerect_8cpp.html',1,'']]],
+  ['drawtransrect_2ecpp_6',['drawtransrect.cpp',['../da/dee/drawtransrect_8cpp.html',1,'']]]
 ];
