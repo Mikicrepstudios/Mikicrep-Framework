7452691d29ab2b73c7b50252712029ddf1ae6e2b
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:18:16 +0100

    Added ability to draw shaking rect

--- miki-framework/mf/graphics.h
+++ miki-framework/mf/graphics.h
@@ -11,8 +11,11 @@
 namespace draw {
     // Core
 	void DrawButton(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int mouseX, int mouseY);
+
 	void DrawRect(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color);
 	void DrawTransRect(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int colorA);
+	void DrawShakingRect(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int intensity);
 	void DrawTextureRect(SDL_Renderer* renderer, SDL_Rect rect, SDL_Texture* texture);
+	
 	void DrawText(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect rect, const char* text, MF_Color color);
 }
