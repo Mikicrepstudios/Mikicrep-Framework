e5e73e811ef5b6962fe2a0993783b1c131e3f274
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:42:48 +0100

    Update docs

--- docs/html/search/files_b.js
+++ docs/html/search/files_b.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['sfx_2eh_0',['sfx.h',['../de/d76/sfx_8h.html',1,'']]]
+  ['setdrawcolor_2ecpp_0',['setdrawcolor.cpp',['../d1/dcc/setdrawcolor_8cpp.html',1,'']]],
+  ['sfx_2eh_1',['sfx.h',['../de/d76/sfx_8h.html',1,'']]]
 ];
--- docs/html/search/functions_a.js
+++ docs/html/search/functions_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['timecount_0',['TimeCount',['../d9/d0f/namespacecore.html#a3d2ea9bae70a098cbe6045b16a95e47c',1,'core']]]
+  ['setdrawcolor_0',['SetDrawColor',['../de/d00/namespacedraw.html#a47ed218c3a1917f6958f6d43247de0b0',1,'draw']]]
 ];
