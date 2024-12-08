7a2b88c237dbc30cb29012fe4aab46baffed57ba
Mikicrep <mikicrepoffical@gmail.com>
2024-12-09 00:07:49 +0100

    Document setwindowicon.cpp

--- miki-framework/core/setwindowicon.cpp
+++ miki-framework/core/setwindowicon.cpp
@@ -4,6 +4,12 @@
 
 namespace core {
     void SetWindowIcon(SDL_Window* window, std::string path) {
+        /**
+         * @brief This sets window icon
+         * @param window Window that will get its icon changed
+         * @param path Path to icon
+         */
+        
         SDL_Surface* iconSurface = IMG_Load(path.c_str());
         SDL_SetWindowIcon(window, iconSurface);
         SDL_FreeSurface(iconSurface);
