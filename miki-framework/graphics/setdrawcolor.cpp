13c84edc4ca651a69abb4e855aeb196dfc6adfab
Mikicrep <mikicrepoffical@gmail.com>
2024-12-08 23:42:40 +0100

    Document setdrawcolor.cpp

--- miki-framework/graphics/setdrawcolor.cpp
+++ miki-framework/graphics/setdrawcolor.cpp
@@ -4,6 +4,12 @@
 
 namespace draw {
 	void SetDrawColor(SDL_Renderer* renderer, MF_Color color) {
+        /**
+         * @brief If you need to change your draw color for some reason, here you have it
+         * @param rendered Window renderer
+         * @param color Color which to be set as draw color
+         */
+        
 		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
 	}
 }
