a35faf50c29e6571b4ad5fd05bb2e7611bff3e3c
Mikicrep <mikicrepoffical@gmail.com>
2024-12-09 00:07:58 +0100

    Update docs

--- docs/html/search/functions_a.js
+++ docs/html/search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['setdrawcolor_0',['SetDrawColor',['../de/d00/namespacedraw.html#a47ed218c3a1917f6958f6d43247de0b0',1,'draw']]]
+  ['setdrawcolor_0',['SetDrawColor',['../de/d00/namespacedraw.html#a47ed218c3a1917f6958f6d43247de0b0',1,'draw']]],
+  ['setwindowicon_1',['SetWindowIcon',['../d9/d0f/namespacecore.html#a1fb511e8957cf8995e78e8609453916f',1,'core']]]
 ];
