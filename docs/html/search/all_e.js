7c7db2f66fea16681f5900a54d3c3597c946854b
Mikicrep <mikicrepoffical@gmail.com>
2024-12-09 00:07:58 +0100

    Update docs

--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -2,7 +2,9 @@ var searchData=
 [
   ['setdrawcolor_0',['SetDrawColor',['../de/d00/namespacedraw.html#a47ed218c3a1917f6958f6d43247de0b0',1,'draw']]],
   ['setdrawcolor_2ecpp_1',['setdrawcolor.cpp',['../d1/dcc/setdrawcolor_8cpp.html',1,'']]],
-  ['sfx_2',['sfx',['../d4/d03/namespacesfx.html',1,'']]],
-  ['sfx_2eh_3',['sfx.h',['../de/d76/sfx_8h.html',1,'']]],
-  ['sky_4',['sky',['../dc/d04/namespacecolors.html#a2612d4f664e6eeaae4531f80b35747c7',1,'colors']]]
+  ['setwindowicon_2',['SetWindowIcon',['../d9/d0f/namespacecore.html#a1fb511e8957cf8995e78e8609453916f',1,'core']]],
+  ['setwindowicon_2ecpp_3',['setwindowicon.cpp',['../d3/da6/setwindowicon_8cpp.html',1,'']]],
+  ['sfx_4',['sfx',['../d4/d03/namespacesfx.html',1,'']]],
+  ['sfx_2eh_5',['sfx.h',['../de/d76/sfx_8h.html',1,'']]],
+  ['sky_6',['sky',['../dc/d04/namespacecolors.html#a2612d4f664e6eeaae4531f80b35747c7',1,'colors']]]
 ];
--- docs/html/search/files_b.js
+++ docs/html/search/files_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['setdrawcolor_2ecpp_0',['setdrawcolor.cpp',['../d1/dcc/setdrawcolor_8cpp.html',1,'']]],
-  ['sfx_2eh_1',['sfx.h',['../de/d76/sfx_8h.html',1,'']]]
+  ['setwindowicon_2ecpp_1',['setwindowicon.cpp',['../d3/da6/setwindowicon_8cpp.html',1,'']]],
+  ['sfx_2eh_2',['sfx.h',['../de/d76/sfx_8h.html',1,'']]]
 ];
