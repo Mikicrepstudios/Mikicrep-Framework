6dfca6f8bdc7a6aec944b7d8470377c2648c73e0
Mikicrep <mikicrepoffical@gmail.com>
2024-12-21 21:59:10 +0100

    Dcoument casttoint.cpp

--- miki-framework/extra/casttoint.cpp
+++ miki-framework/extra/casttoint.cpp
@@ -1,5 +1,9 @@
 namespace extra {
-    int castToInt(double value) {
+    int castToInt(float value) {
+        /**
+         * @brief This function converts int number to float
+         * @param value Number to convert
+         */
         return static_cast<int>(value);
     }
 }
