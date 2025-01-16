bc068f9a79853d351f28996cdc48e9af25440a90
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:11:41 +0100

    DrawCircle() function

--- miki-framework/graphics/drawcircle.cpp
+++ miki-framework/graphics/drawcircle.cpp
@@ -1,21 +1,27 @@
-void drawFilledCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
-    int x = 0;
-    int y = radius;
-    int decision = 1 - radius;
+#include <SDL.h>
 
-    while (x <= y) {
-        // Draw horizontal lines to fill the circle
-        DrawLine(renderer, centerX - x, centerY + y, centerX + x, centerY + y);
-        DrawLine(renderer, centerX - x, centerY - y, centerX + x, centerY - y);
-        DrawLine(renderer, centerX - y, centerY + x, centerX + y, centerY + x);
-        DrawLine(renderer, centerX - y, centerY - x, centerX + y, centerY - x);
+#include <mf/graphics.h>
 
-        if (decision < 0) {
-            decision += 2 * x + 3;
-        } else {
-            decision += 2 * (x - y) + 5;
-            y--;
+namespace draw {
+    void DrawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+        int x = 0;
+        int y = radius;
+        int decision = 1 - radius;
+
+        while (x <= y) {
+            // Draw horizontal lines to fill the circle
+            DrawLine(renderer, centerX - x, centerY + y, centerX + x, centerY + y);
+            DrawLine(renderer, centerX - x, centerY - y, centerX + x, centerY - y);
+            DrawLine(renderer, centerX - y, centerY + x, centerX + y, centerY + x);
+            DrawLine(renderer, centerX - y, centerY - x, centerX + y, centerY - x);
+
+            if (decision < 0) {
+                decision += 2 * x + 3;
+            } else {
+                decision += 2 * (x - y) + 5;
+                y--;
+            }
+            x++;
         }
-        x++;
     }
 }
