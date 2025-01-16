06b1e2a21ed83447689ba4d6761a019019d38c7b
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:01:27 +0100

    Update docs

--- docs/html/search/files_2.js
+++ docs/html/search/files_2.js
@@ -2,9 +2,11 @@ var searchData=
 [
   ['deletefile_2ecpp_0',['deletefile.cpp',['../d0/dae/deletefile_8cpp.html',1,'']]],
   ['drawbutton_2ecpp_1',['drawbutton.cpp',['../da/d6c/drawbutton_8cpp.html',1,'']]],
-  ['drawrect_2ecpp_2',['drawrect.cpp',['../d1/da5/drawrect_8cpp.html',1,'']]],
-  ['drawshakingrect_2ecpp_3',['drawshakingrect.cpp',['../d5/d8c/drawshakingrect_8cpp.html',1,'']]],
-  ['drawtext_2ecpp_4',['drawtext.cpp',['../d7/dc8/drawtext_8cpp.html',1,'']]],
-  ['drawtexturerect_2ecpp_5',['drawtexturerect.cpp',['../db/dbf/drawtexturerect_8cpp.html',1,'']]],
-  ['drawtransrect_2ecpp_6',['drawtransrect.cpp',['../da/dee/drawtransrect_8cpp.html',1,'']]]
+  ['drawcircle_2ecpp_2',['drawcircle.cpp',['../d6/d9c/drawcircle_8cpp.html',1,'']]],
+  ['drawpixel_2ecpp_3',['drawpixel.cpp',['../d6/dff/drawpixel_8cpp.html',1,'']]],
+  ['drawrect_2ecpp_4',['drawrect.cpp',['../d1/da5/drawrect_8cpp.html',1,'']]],
+  ['drawshakingrect_2ecpp_5',['drawshakingrect.cpp',['../d5/d8c/drawshakingrect_8cpp.html',1,'']]],
+  ['drawtext_2ecpp_6',['drawtext.cpp',['../d7/dc8/drawtext_8cpp.html',1,'']]],
+  ['drawtexturerect_2ecpp_7',['drawtexturerect.cpp',['../db/dbf/drawtexturerect_8cpp.html',1,'']]],
+  ['drawtransrect_2ecpp_8',['drawtransrect.cpp',['../da/dee/drawtransrect_8cpp.html',1,'']]]
 ];
