6c59d7659c7ba9a28e001e972c20a4f9b1aa7852
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:31:16 +0100

    Redocument drawline.cpp

--- miki-framework/graphics/drawline.cpp
+++ miki-framework/graphics/drawline.cpp
@@ -6,10 +6,8 @@ namespace draw {
     void DrawLine(SDL_Renderer* renderer, MF_Pixel start, MF_Pixel end) {
         /**
          * @brief Draws line
-         * @param x1 X coordinate of the first point
-         * @param y1 Y coordinate of the first point
-         * @param x2 X coordinate of the second point
-         * @param y2 Y coordinate of the second point
+         * @param start Start point
+         * @param end End point
          */
         
         SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
