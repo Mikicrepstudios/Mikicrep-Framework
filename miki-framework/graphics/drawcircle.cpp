db7be5306d4683797aa238f83bcb052632432a76
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:10:31 +0100

    Include DrawLine() in graphics.h

---
+++ miki-framework/graphics/drawcircle.cpp
@@ -0,0 +1,21 @@
+void drawFilledCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
+    int x = 0;
+    int y = radius;
+    int decision = 1 - radius;
+
+    while (x <= y) {
+        // Draw horizontal lines to fill the circle
+        DrawLine(renderer, centerX - x, centerY + y, centerX + x, centerY + y);
+        DrawLine(renderer, centerX - x, centerY - y, centerX + x, centerY - y);
+        DrawLine(renderer, centerX - y, centerY + x, centerX + y, centerY + x);
+        DrawLine(renderer, centerX - y, centerY - x, centerX + y, centerY - x);
+
+        if (decision < 0) {
+            decision += 2 * x + 3;
+        } else {
+            decision += 2 * (x - y) + 5;
+            y--;
+        }
+        x++;
+    }
+}
