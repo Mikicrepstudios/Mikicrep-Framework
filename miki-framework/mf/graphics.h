45175d7b3ae43c0d201f25cfedba71efa0b489a8
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:29:05 +0100

    Update drawline in graphics.h

--- miki-framework/mf/graphics.h
+++ miki-framework/mf/graphics.h
@@ -12,7 +12,7 @@ namespace draw {
     // Core
 	void SetDrawColor(SDL_Renderer* renderer, MF_Color color);
 	void DrawPixel(SDL_Renderer* renderer, int x, int y);
-	void DrawLine(SDL_Renderer* renderer, int x1, int y1, int x2, int y2);
+	void DrawLine(SDL_Renderer* renderer, MF_Pixel start, MF_Pixel end);
 
 	void DrawButton(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int mouseX, int mouseY);
 
