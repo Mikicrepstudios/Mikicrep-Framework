0a0f4e020c6f85a4a2043dcad57a7a0a4d79d87e
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 19:51:55 +0100

    DrawCircle() function

--- miki-framework/mf/graphics.h
+++ miki-framework/mf/graphics.h
@@ -15,10 +15,14 @@ namespace draw {
 
 	void DrawButton(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int mouseX, int mouseY);
 
+	// Rectangle
 	void DrawRect(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color);
 	void DrawTransRect(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int colorA);
 	void DrawShakingRect(SDL_Renderer* renderer, SDL_Rect rect, MF_Color color, int intensity);
 	void DrawTextureRect(SDL_Renderer* renderer, SDL_Rect rect, SDL_Texture* texture);
+
+	// Circle
+	void DrawCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius);
 	
 	void DrawText(SDL_Renderer* renderer, TTF_Font* font, SDL_Rect rect, const char* text, MF_Color color);
 }
