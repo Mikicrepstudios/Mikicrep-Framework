c93dedd984c80fdd3ec582cbc715aaaafe47a9ae
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 19:49:37 +0100

    Namespace

--- miki-framework/graphics/drawpixel.cpp
+++ miki-framework/graphics/drawpixel.cpp
@@ -1,5 +1,7 @@
 #include <SDL.h>
 
-void DrawPixel(SDL_Renderer* renderer, int x, int y) {
-    SDL_RenderDrawPoint(renderer, x, y);
+namespace graphics {
+    void DrawPixel(SDL_Renderer* renderer, int x, int y) {
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
 }
