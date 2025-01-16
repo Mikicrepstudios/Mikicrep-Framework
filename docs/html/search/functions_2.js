97f6390b3abc07107c6ad4ee1bb26d393bb03ced
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:01:27 +0100

    Update docs

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -2,9 +2,11 @@ var searchData=
 [
   ['deletefile_0',['DeleteFile',['../d6/d04/namespacefiles.html#a6fa94beb256aab79f0c4c86dc82f62a0',1,'files']]],
   ['drawbutton_1',['DrawButton',['../de/d00/namespacedraw.html#a6bb79d11848aa97911775cdd12fca90f',1,'draw']]],
-  ['drawrect_2',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
-  ['drawshakingrect_3',['DrawShakingRect',['../de/d00/namespacedraw.html#a75f2625456c6793af020760ff869bb70',1,'draw']]],
-  ['drawtext_4',['DrawText',['../de/d00/namespacedraw.html#a578bc027371215cd91e20b154a7fd18c',1,'draw']]],
-  ['drawtexturerect_5',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
-  ['drawtransrect_6',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
+  ['drawcircleborder_2',['DrawCircleBorder',['../de/d00/namespacedraw.html#af88c7fbf03a209ccde9f0e0f70ac498d',1,'draw']]],
+  ['drawpixel_3',['DrawPixel',['../de/d00/namespacedraw.html#a2cdfd1c2a98504489058e29e64c1c589',1,'draw']]],
+  ['drawrect_4',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
+  ['drawshakingrect_5',['DrawShakingRect',['../de/d00/namespacedraw.html#a75f2625456c6793af020760ff869bb70',1,'draw']]],
+  ['drawtext_6',['DrawText',['../de/d00/namespacedraw.html#a578bc027371215cd91e20b154a7fd18c',1,'draw']]],
+  ['drawtexturerect_7',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
+  ['drawtransrect_8',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
 ];
