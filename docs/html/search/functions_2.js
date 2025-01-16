b7803258a150985752a8c3801e4e3094b71d032c
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:16:57 +0100

    Update docs

--- docs/html/search/functions_2.js
+++ docs/html/search/functions_2.js
@@ -2,12 +2,13 @@ var searchData=
 [
   ['deletefile_0',['DeleteFile',['../d6/d04/namespacefiles.html#a6fa94beb256aab79f0c4c86dc82f62a0',1,'files']]],
   ['drawbutton_1',['DrawButton',['../de/d00/namespacedraw.html#a6bb79d11848aa97911775cdd12fca90f',1,'draw']]],
-  ['drawcircleborder_2',['DrawCircleBorder',['../de/d00/namespacedraw.html#af88c7fbf03a209ccde9f0e0f70ac498d',1,'draw']]],
-  ['drawline_3',['DrawLine',['../de/d00/namespacedraw.html#ad72e0c7ec6aec785d9e6bda8c369e7d9',1,'draw']]],
-  ['drawpixel_4',['DrawPixel',['../de/d00/namespacedraw.html#a2cdfd1c2a98504489058e29e64c1c589',1,'draw']]],
-  ['drawrect_5',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
-  ['drawshakingrect_6',['DrawShakingRect',['../de/d00/namespacedraw.html#a75f2625456c6793af020760ff869bb70',1,'draw']]],
-  ['drawtext_7',['DrawText',['../de/d00/namespacedraw.html#a578bc027371215cd91e20b154a7fd18c',1,'draw']]],
-  ['drawtexturerect_8',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
-  ['drawtransrect_9',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
+  ['drawcircle_2',['DrawCircle',['../de/d00/namespacedraw.html#aa1dba2c233e7c94de8074985cc97ff42',1,'draw']]],
+  ['drawcircleborder_3',['DrawCircleBorder',['../de/d00/namespacedraw.html#af88c7fbf03a209ccde9f0e0f70ac498d',1,'draw']]],
+  ['drawline_4',['DrawLine',['../de/d00/namespacedraw.html#ad72e0c7ec6aec785d9e6bda8c369e7d9',1,'draw']]],
+  ['drawpixel_5',['DrawPixel',['../de/d00/namespacedraw.html#a2cdfd1c2a98504489058e29e64c1c589',1,'draw']]],
+  ['drawrect_6',['DrawRect',['../de/d00/namespacedraw.html#a8a13233777eb544ff1d2b092b172db83',1,'draw']]],
+  ['drawshakingrect_7',['DrawShakingRect',['../de/d00/namespacedraw.html#a75f2625456c6793af020760ff869bb70',1,'draw']]],
+  ['drawtext_8',['DrawText',['../de/d00/namespacedraw.html#a578bc027371215cd91e20b154a7fd18c',1,'draw']]],
+  ['drawtexturerect_9',['DrawTextureRect',['../de/d00/namespacedraw.html#a39058e0c15c04c4e29db0d905ac2b44c',1,'draw']]],
+  ['drawtransrect_10',['DrawTransRect',['../de/d00/namespacedraw.html#a5c49fd83b7ff6092c47acfb0cbc72c19',1,'draw']]]
 ];
