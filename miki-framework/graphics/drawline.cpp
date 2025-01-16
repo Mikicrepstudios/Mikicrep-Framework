a8a88c05cec1c2555bcc2bc8899f48887ed25e3e
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:07:23 +0100

    Document drawline.cpp

--- miki-framework/graphics/drawline.cpp
+++ miki-framework/graphics/drawline.cpp
@@ -2,6 +2,14 @@
 
 namespace draw {
     void DrawLine(SDL_Renderer* renderer, int x1, int y1, int x2, int y2) {
+        /**
+         * @brief Draws line
+         * @param x1 X coordinate of the first point
+         * @param y1 Y coordinate of the first point
+         * @param x2 X coordinate of the second point
+         * @param y2 Y coordinate of the second point
+         */
+        
         SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
     }
 }
