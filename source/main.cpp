02c0d0deac88118fd4cafafbba7e756e9f7460af
Mikicrep <mikicrepoffical@gmail.com>
2025-01-16 20:09:26 +0100

    Edit comment

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
     bool running = true;
 
     // MUST USE LINE BELOW
-    //core::printver("1.5.0", 1);
+    //core::printver("1.6.0", 1);
     core::printver("", 3); // DEV Version
 
     // Main stuff
