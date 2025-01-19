fe01c954f4ccfca11091ef699e5232d4b481926c
Mikicrep <mikicrepoffical@gmail.com>
2025-01-19 11:36:09 +0100

    1.6.0

--- source/main.cpp
+++ source/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
     bool running = true;
 
     // MUST USE LINE BELOW
-    core::printver(3);
+    core::printver(1);
 
     // Main stuff
     const std::string title = "Mikicrep Framework";
