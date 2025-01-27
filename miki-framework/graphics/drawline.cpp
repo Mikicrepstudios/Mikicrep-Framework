1c7b5bcaf4788c6e093d6ca74909d139c24a76b5
Mikicrep <mikicrepoffical@gmail.com>
2025-01-27 22:19:25 +0100

    Add color and thickness to drawline

--- miki-framework/graphics/drawline.cpp
+++ miki-framework/graphics/drawline.cpp
@@ -1,15 +1,20 @@
 #include <SDL.h>
 
-#include <mf/core.h>
+#include "mf/core.h"
+#include "mf/graphics.h"
 
 namespace draw {
-    void DrawLine(SDL_Renderer* renderer, MF_Pixel start, MF_Pixel end) {
+    void DrawLine(SDL_Renderer* renderer, MF_Pixel start, MF_Pixel end, MF_Color color, int thickness) {
         /**
          * @brief Draws line
          * @param start Start point
          * @param end End point
          */
         
-        SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
+        draw::SetDrawColor(renderer, color);
+        
+		for (int i = -thickness / 2; i <= thickness / 2; ++i) {
+        	SDL_RenderDrawLine(renderer, start.x, start.y, end.x, end.y);
+    	}
     }
 }
