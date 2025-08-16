9cc590db24a460db8227b0ff9046ae6c5519171e
Mikicrep <mikicrepoffical@gmail.com>
2025-08-16 20:16:24 +0200

    Make button hover effect weaker

--- miki-framework/graphics/drawbutton.cpp
+++ miki-framework/graphics/drawbutton.cpp
@@ -16,9 +16,9 @@ namespace draw {
 		// If mouse hovers button then darken it, if not then dont darken
 		if (mouseX >= rect.x && mouseX <= rect.x + rect.w &&
 			mouseY >= rect.y && mouseY <= rect.y + rect.h) {
-			color.r *= .25;
-			color.g *= .25;
-			color.b *= .25;
+			color.r *= .65;
+			color.g *= .65;
+			color.b *= .65;
 			SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
 			SDL_RenderFillRect(renderer, &rect);
 		}
